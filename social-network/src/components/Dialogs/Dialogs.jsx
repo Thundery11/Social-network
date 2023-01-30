65c92175a96ef189d1f9bfe0d936d3c4cbb08ad4
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-01-30 17:01:42 +0300

    feat: add routes for dialogs

--- social-network/src/components/Dialogs/Dialogs.jsx
+++ social-network/src/components/Dialogs/Dialogs.jsx
@@ -1,6 +1,37 @@
+import { NavLink } from "react-router-dom";
 import s from "./Dialogs.module.css";
 const Dialogs = (props) => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/1">Ilya</NavLink>{" "}
+        </div>
+        <div className={`${s.dialog} ${s.active}`}>
+          <NavLink to="/dialogs/2">Katya</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/3">Sasha</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/4">Dasha</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/5">Jenya</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/6">Denis</NavLink>
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>Yo</div>
+        <div className={s.message}>alright</div>
+        <div className={s.message}>Hey</div>
+        <div className={s.message}>Bye</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
