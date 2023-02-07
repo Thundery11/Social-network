596135cfd0ed3a65d2b7d8e96e143e865916fc49
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-02-07 20:18:28 +0300

    Update MyPosts.jsx

--- social-network/src/components/Profile/MyPosts/MyPosts.jsx
+++ social-network/src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,18 +1,24 @@
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
+import React from "react";
 const MyPosts = (props) => {
   let postsElements = props.posts.map((p) => (
     <Post message={p.message} id={p.id} likesCount={p.likesCount} />
   ));
+  let newPostElement = React.createRef();
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text);
+  };
   return (
     <div className={s.postsBlock}>
       <h3>My posts</h3>
       <div>
         <div>
-          <textarea></textarea>
+          <textarea ref={newPostElement}></textarea>
         </div>
         <div>
-          <button onClick={() => {}}>Add post</button>
+          <button onClick={addPost}>Add post</button>
         </div>
         <button>Remove</button>
       </div>
