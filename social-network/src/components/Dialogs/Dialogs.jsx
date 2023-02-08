2fe34cb0d277e3d3c02797b8946ad38c5f4bfcb1
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-02-08 17:50:11 +0300

    Update Dialogs.jsx

--- social-network/src/components/Dialogs/Dialogs.jsx
+++ social-network/src/components/Dialogs/Dialogs.jsx
@@ -2,6 +2,7 @@ import { NavLink } from "react-router-dom";
 import DialogItem from "./DialogItem/DialogItem";
 import s from "./Dialogs.module.css";
 import Message from "./Message/Message";
+import React from "react";
 
 const Dialogs = (props) => {
   let dialogsElements = props.state.dialogs.map((d) => (
@@ -11,10 +12,24 @@ const Dialogs = (props) => {
   let messagesElements = props.state.messages.map((m) => (
     <Message message={m.message} id={m.id} />
   ));
+  let newDialogElement = React.createRef();
+  let addDialog = () => {
+    let text = newDialogElement.current.value;
+    alert(text);
+  };
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>{dialogsElements}</div>
       <div className={s.messages}>{messagesElements}</div>
+      <div>
+        <div>
+          <textarea ref={newDialogElement}></textarea>
+        </div>
+        <div>
+          <button onClick={addDialog}>Add post</button>
+        </div>
+        <button>Remove</button>
+      </div>
     </div>
   );
 };
