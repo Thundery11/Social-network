6c5b111009bb66390ee7d885fc397e5d4868c995
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-02-15 20:05:16 +0300

    feat: change if/else for switch/case in reducers

--- social-network/src/redux/dialogs-reducer.js
+++ social-network/src/redux/dialogs-reducer.js
@@ -2,17 +2,21 @@ const ADD_MESSAGE = "ADD-MESSAGE";
 const UPDATE_NEW_MESSAGE_TEXT = "UPDATE-NEW-MESSAGE-TEXT";
 
 let dialogsReducer = (state, action) => {
-  if (action.type === ADD_MESSAGE) {
-    let newMessage = {
-      message: state.newMessageText,
-      id: 4,
-    };
-    state.messages.push(newMessage);
-    state.newMessageText = " ";
-  } else if (action.type === UPDATE_NEW_MESSAGE_TEXT) {
-    state.newMessageText = action.newMessage;
+  switch (action.type) {
+    case ADD_MESSAGE:
+      let newMessage = {
+        message: state.newMessageText,
+        id: 4,
+      };
+      state.messages.push(newMessage);
+      state.newMessageText = " ";
+      return state;
+    case UPDATE_NEW_MESSAGE_TEXT:
+      state.newMessageText = action.newMessage;
+      return state;
+    default:
+      return state;
   }
-  return state;
 };
 
 export default dialogsReducer;
