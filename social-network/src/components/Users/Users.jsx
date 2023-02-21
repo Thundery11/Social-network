ac19b2baeebccc2f5e627e30371f45417ca409bc
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-02-21 15:56:00 +0300

    api

--- social-network/src/components/Users/Users.jsx
+++ social-network/src/components/Users/Users.jsx
@@ -1,12 +1,25 @@
 import s from "./Users.module.css";
+import axios from "axios";
+import userPhoto from "../../assets/images/default-user.jpg";
 let Users = (props) => {
+  if (props.users.length === 0) {
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
+        props.setUsers(response.data.items);
+      });
+  }
   return (
     <div>
       {props.users.map((u) => (
         <div key={u.id}>
           <span>
             <div>
-              <img src={u.photoUrl} className={s.userPhoto} />
+              <img
+                src={u.photos.small != null ? u.photos.small : userPhoto}
+                alt="sdsds"
+                className={s.userPhoto}
+              />
             </div>
             <div>
               {u.followed ? (
@@ -31,12 +44,12 @@ let Users = (props) => {
           </span>
           <span>
             <span>
-              <div>{u.fullname}</div>
+              <div>{u.name}</div>
               <div>{u.status}</div>
             </span>
             <span>
-              <div>{u.location.country}</div>
-              <div>{u.location.city}</div>
+              <div>{"u.location.country"}</div>
+              <div>{"u.location.city"}</div>
             </span>
           </span>
         </div>
--- social-network/src/redux/users-reducer.js
+++ social-network/src/redux/users-reducer.js
@@ -3,51 +3,7 @@ const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET_USERS";
 
 let initialState = {
-  users: [
-    {
-      id: 1,
-      fullName: "Illia",
-      status: "I`m starting developer",
-      location: { city: "Mogilev", county: "Belarus" },
-      photoUrl:
-        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.hockeydb.com%2Fihdb%2Fstats%2Fpdisplay.php%3Fpid%3D56745&psig=AOvVaw3hOeA3wqpK14c8JpCAiSMi&ust=1677062297583000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLjVgoe2pv0CFQAAAAAdAAAAABAE",
-      followed: true,
-    },
-    {
-      id: 2,
-      fullName: "Katya",
-      status: "I`m girl",
-      location: { city: "Mogilev", county: "Belarus" },
-      photoUrl:
-        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FAlexander_Lukashenko&psig=AOvVaw26zHE8Ym4g35PrPP3aISRl&ust=1677062869603000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCNjHzJe4pv0CFQAAAAAdAAAAABAE",
-      followed: true,
-    },
-    {
-      id: 3,
-      fullName: "Denis",
-      status: "developer",
-      location: { city: "Mogilev", county: "Belarus" },
-      photoUrl:
-        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.hockeydb.com%2Fihdb%2Fstats%2Fpdisplay.php%3Fpid%3D56745&psig=AOvVaw3hOeA3wqpK14c8JpCAiSMi&ust=1677062297583000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLjVgoe2pv0CFQAAAAAdAAAAABAE",
-      followed: false,
-    },
-    {
-      id: 4,
-      fullName: "Sasha",
-      status: "basketball player",
-      location: { city: "Mogilev", county: "Belarus" },
-      photoUrl:
-        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.hockeydb.com%2Fihdb%2Fstats%2Fpdisplay.php%3Fpid%3D56745&psig=AOvVaw3hOeA3wqpK14c8JpCAiSMi&ust=1677062297583000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLjVgoe2pv0CFQAAAAAdAAAAABAE",
-      followed: true,
-    },
-    {
-      id: 5,
-      fullName: "Jenya",
-      status: "staring developer",
-      location: { city: "Mogilev", county: "Belarus" },
-      followed: true,
-    },
-  ],
+  users: [],
 };
 
 const usersReducer = (state = initialState, action) => {
