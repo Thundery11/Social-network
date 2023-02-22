12e4da52970faaafe89d91c810efc65343a3f07e
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-02-22 17:48:11 +0300

    Update Users.jsx

--- social-network/src/components/Users/Users.jsx
+++ social-network/src/components/Users/Users.jsx
@@ -2,15 +2,18 @@ import s from "./Users.module.css";
 import axios from "axios";
 import userPhoto from "../../assets/images/default-user.jpg";
 let Users = (props) => {
-  if (props.users.length === 0) {
-    axios
-      .get("https://social-network.samuraijs.com/api/1.0/users")
-      .then((response) => {
-        props.setUsers(response.data.items);
-      });
-  }
+  let getUsers = () => {
+    if (props.users.length === 0) {
+      axios
+        .get("https://social-network.samuraijs.com/api/1.0/users")
+        .then((response) => {
+          props.setUsers(response.data.items);
+        });
+    }
+  };
   return (
     <div>
+      <button onClick={getUsers}>Get users</button>
       {props.users.map((u) => (
         <div key={u.id}>
           <span>
