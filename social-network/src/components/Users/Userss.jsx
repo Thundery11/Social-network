1767b0db618e8c7af54a827d2ac9578f094225b1
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-02-22 18:43:47 +0300

    feat: add class component Users

---
+++ social-network/src/components/Users/Userss.jsx
@@ -0,0 +1,64 @@
+import s from "./Users.module.css";
+import axios from "axios";
+import userPhoto from "../../assets/images/default-user.jpg";
+let Users = (props) => {
+  let getUsers = () => {
+    if (props.users.length === 0) {
+      axios
+        .get("https://social-network.samuraijs.com/api/1.0/users")
+        .then((response) => {
+          props.setUsers(response.data.items);
+        });
+    }
+  };
+  return (
+    <div>
+      <button onClick={getUsers}>Get users</button>
+      {props.users.map((u) => (
+        <div key={u.id}>
+          <span>
+            <div>
+              <img
+                src={u.photos.small != null ? u.photos.small : userPhoto}
+                alt="sdsds"
+                className={s.userPhoto}
+              />
+            </div>
+            <div>
+              {u.followed ? (
+                <button
+                  onClick={() => {
+                    props.unfollow(u.id);
+                  }}
+                >
+                  Follow
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    props.follow(u.id);
+                  }}
+                >
+                  Unfollow
+                </button>
+              )}
+              {/* <button>Follow</button> */}
+            </div>
+          </span>
+          <span>
+            <span>
+              <div>{u.name}</div>
+              <div>{u.status}</div>
+            </span>
+            <span>
+              <div>{"u.location.country"}</div>
+              <div>{"u.location.city"}</div>
+            </span>
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Users;
