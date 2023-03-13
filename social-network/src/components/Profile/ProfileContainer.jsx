450b6bc48d8bd9bb3defaca5c21d542a8c6210dc
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-03-13 17:27:12 +0300

    feat: add WithRouter

--- social-network/src/components/Profile/ProfileContainer.jsx
+++ social-network/src/components/Profile/ProfileContainer.jsx
@@ -3,11 +3,19 @@ import React from "react";
 import axios from "axios";
 import { connect } from "react-redux";
 import { setUserProfile } from "../../redux/profile-reducer";
+import { useParams } from "react-router-dom";
 
+export function withRouter(Children) {
+  return (props) => {
+    const match = { params: useParams() };
+    return <Children {...props} match={match} />;
+  };
+}
 class ProfileContainer extends React.Component {
   componentDidMount() {
+    let userId = this.props.match.params.userId;
     axios
-      .get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+      .get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
       .then((response) => {
         this.props.setUserProfile(response.data);
       });
@@ -25,5 +33,8 @@ let mapStateToProps = (state) => {
     profile: state.profilePage.profile,
   };
 };
+let withUrlDataContainerComponent = withRouter(ProfileContainer);
 
-export default connect(mapStateToProps, { setUserProfile })(ProfileContainer);
+export default connect(mapStateToProps, { setUserProfile })(
+  withUrlDataContainerComponent
+);
