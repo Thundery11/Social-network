5123b50ada260ad9e78a0f9107d5265dcd49fb75
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-04-03 19:28:07 +0300

    feat: added LoginForm

--- social-network/src/Login/Login.jsx
+++ social-network/src/Login/Login.jsx
@@ -1,4 +1,44 @@
-const Login = (props) => {
-  return <h1>Login</h1>;
+import { Field, reduxForm } from "redux-form";
+
+const LoginForm = (props) => {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <div>
+        <label htmlFor="Login">Login</label>
+        <Field name="Login" placeholder="login" component="input" type="text" />
+      </div>
+      <div>
+        <label htmlFor="Password">Password</label>
+        <Field
+          name="Password"
+          placeholder="password"
+          component="input"
+          type="text"
+        />
+      </div>
+      <div>
+        <Field name="rememberMe" component="input" type="checkbox" />
+        remember me
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
 };
+
+const LoginReduxForm = reduxForm({
+  form: "login",
+})(LoginForm);
+
+const Login = (props) => {
+  const onSubmit = (formData) => {
+    console.log(formData);
+  };
+  return (
+    <>
+      <h1>Login</h1>
+      <LoginReduxForm onSubmit={onSubmit} />
+    </>
+  );
+};
+
 export default Login;
--- social-network/src/redux/redux-store.js
+++ social-network/src/redux/redux-store.js
@@ -8,11 +8,13 @@ import dialogsReducer from "./dialogs-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import ThunkMiddleware from "redux-thunk";
+import { reducer as formReducer } from "redux-form";
 let reducers = combineReducers({
   profilePage: profileReducer,
   dialogsPage: dialogsReducer,
   usersPage: usersReducer,
   auth: authReducer,
+  form: formReducer,
 });
 
 let store = createStore(reducers, applyMiddleware(ThunkMiddleware));
